31ca10e7472a4a452d47a6346d3b62c8d00572e0
abdulla1395 <abdullahfx95@gmail.com>
2025-07-05 21:17:43 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>مرحبا بك في قولها</h1>
+      <p>منصة توليد شخصيات عربية تتكلم باللهجات المحلية</p>
+    </div>
+  );
+}
